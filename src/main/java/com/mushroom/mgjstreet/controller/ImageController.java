b900b144eb70a940b878949045ac2c0bb1165f2d
kakapo <936555313@qq.com>
2021-01-20 14:32:33 +0800

    2021年1月20日

---
+++ src/main/java/com/mushroom/mgjstreet/controller/ImageController.java
@@ -0,0 +1,28 @@
+package com.mushroom.mgjstreet.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+@RequestMapping("api")
+public class ImageController {
+
+    @PostMapping("/UploadImage")
+    public String UploadImage(@RequestParam(name = "Image") MultipartFile ImageFile, HttpServletRequest httpServletRequest){
+        String format = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String RealPath = httpServletRequest.getServletContext().getRealPath("/");
+        long size = ImageFile.getSize();
+        try {
+            InputStream inputStream = ImageFile.getInputStream();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println(size);
+        return ImageFile.getOriginalFilename();
+    }
+}
